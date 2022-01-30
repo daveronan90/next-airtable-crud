c44b3c59a266b388ee4a9702516d7f53944cae01
daveronan90 <daveronan90@gmail.com>
2022-01-30 20:50:45 +0000

    Add next links

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,22 +1,22 @@
+import Link from "next/link";
+
 function Navbar({ user }) {
   return (
     <nav className="flex justify-between items-center py-4">
       <h1 className="text-2xl font-bold text-gray-800">Action Tracker</h1>
       <div className="flex">
         {user ? (
-          <a
-            href="/api/auth/logout"
-            className="rounded bg-blue-500 hover:bg-blue-600 text-white py-2 px-4"
-          >
-            Logout
-          </a>
+          <Link href="/api/auth/logout">
+            <a className="rounded bg-blue-500 hover:bg-blue-600 text-white py-2 px-4">
+              Logout
+            </a>
+          </Link>
         ) : (
-          <a
-            href="/api/auth/login"
-            className="rounded bg-blue-500 hover:bg-blue-600 text-white py-2 px-4"
-          >
-            Login
-          </a>
+          <Link href="/api/auth/login">
+            <a className="rounded bg-blue-500 hover:bg-blue-600 text-white py-2 px-4">
+              Login
+            </a>
+          </Link>
         )}
       </div>
     </nav>
